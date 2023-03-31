3b42c0f3c0561f1c967677abb03a099084301e93
TFW <1809075986@qq.com>
2023-03-31 11:04:57 +0800

    1

--- configs/_test_/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_balloon.py
+++ configs/_test_/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_balloon.py
@@ -1,51 +1,78 @@
-_base_ = '../mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_coco.py'
+_base_ = [
+    '../_base_/models/mask_rcnn_r50_fpn.py',
+    '../_base_/datasets/coco_instance.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
 
 debug = False
 
 dataset_type = 'CocoDataset'
-classes = ('熔珠', '爆点', '熔珠-向内溢出-无焊黑', '熔珠-向外溢出-无焊黑', '轻微熔钉', '熔珠-向内溢出-焊黑', '焊黑', '熔珠-锡渣', '存疑', 'OK', '熔珠-焊缝边缘', '电解液', '针孔', '焊灰', '熔钉')
+classes = ('内插', '负极耳OK', '正极耳OK')
 
-data_root = 'data/tfw/KUNKUN2/z/'
-# optimizer = dict(type='Adam', lr=0.01, betas=(0.9, 0.999), eps=1e-8)
-# optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
-# optimizer = dict(
-#     _delete_=True,
-#     type='AdamW',
-#     lr=0.001,
-#     weight_decay=0.05,
-#     paramwise_cfg=dict(norm_decay_mult=0., bypass_duplicate=True))
-# lr_config = dict(
-#     policy='step',
-#     warmup='linear',
-#     warmup_iters=500,
-#     warmup_ratio=0.001,
-#     step=[8, 11])
-
-# auto_scale_lr = dict(enable=False, base_batch_size=16)
+data_root = 'data/tfw/YKUNKUN/'
 
 model = dict(
     roi_head=dict(
         bbox_head=dict(num_classes=len(classes)),
         mask_head=dict(num_classes=len(classes))))
 
+img_scale = (1560, 1560)
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(
+        type='Resize',
+        img_scale=img_scale,
+        ratio_range=[0.75, 1.25],
+        keep_ratio=True),
+    dict(type='RandomCrop', crop_size=img_scale),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=img_scale,
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
 data = dict(
-    persistent_workers=False if debug else True,
-    samples_per_gpu=1,
-    workers_per_gpu=1 if debug else 4,
+    # persistent_workers=False if debug else True,
+    # samples_per_gpu=1,
+    # workers_per_gpu=1 if debug else 4,
     train=dict(
+        pipeline=train_pipeline,
         ann_file=data_root + 'train.json',
         classes=classes,
         img_prefix=data_root + 'train/'),
     val=dict(
+        pipeline=test_pipeline,
         ann_file=data_root + 'val.json',
         classes=classes,
         img_prefix=data_root + 'val/'),
     test=dict(
+        pipeline=test_pipeline,
         ann_file=data_root + 'val.json',
         classes=classes,
         img_prefix=data_root + 'val/'))
 
 # 我们可以使用预训练的 Mask R-CNN 来获取更好的性能
-load_from = 'checkpoints/mask_base3.pth'
-
-runner = dict(type='EpochBasedRunner', max_epochs=12)
+# load_from = 'checkpoints/base_mask7.pth'
+auto_scale_lr = dict(enable=True, base_batch_size=16)
+# runner = dict(type='EpochBasedRunner', max_epochs=12)
